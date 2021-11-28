62ef1c945b16454aef458e1b592c86e98ffa7bc8
Circus-Pygargus <richard.meuret@laposte.net>
2021-11-28 18:56:23 +0100

    [MONSTER] Création de la route /monsters/{slug}

--- src/Controller/MonsterController.php
+++ src/Controller/MonsterController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CategoryController extends AbstractController
+class MonsterController extends AbstractController
 {
     /**
-     * @Route("/categories/{slug}", name="app_categories")
+     * @Route("/monsters/{slug}", name="app_monsters")
      */
     public function index (string $slug='all'): Response
     {
-        return $this->render('category/index.html.twig');
+        return $this->render('monster/index.html.twig');
     }
 }
