9e3a087e21ec053934c78da119fc904ca011dd2f
Circus-Pygargus <richard.meuret@laposte.net>
2021-11-28 16:29:31 +0100

    [IMAGES] Création de la route /admin/images/list : contrôleur, vue et rôles

---
+++ src/Controller/Admin/ImageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class ImageController
+ * @package App\Controller\Admin
+ * @Route("/admin/images", name="admin_images_")
+ */
+class ImageController extends AbstractController
+{
+    /**
+     * @Route("list", name="list")
+     */
+    public function list (): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_CONTRIBUTOR');
+
+        return $this->render('admin/image/list.html.twig');
+    }
+}
