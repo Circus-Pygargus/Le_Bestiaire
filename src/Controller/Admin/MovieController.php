7f4deccc807f1e20dad8e8c6a5653879a67c2611
Circus-Pygargus <richard.meuret@laposte.net>
2021-11-28 17:41:11 +0100

    [MOVIES] Création de la route /admin/movies/list : contrôleur, vue et rôles

---
+++ src/Controller/Admin/MovieController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class MonsterController
+ * @package App\Controller\Admin
+ * @Route("/admin/movies", name="admin_movies_")
+ */
+class MovieController extends AbstractController
+{
+    /**
+     * @Route("/list", name="list")
+     */
+    public function list (): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_CONTRIBUTOR');
+
+        return $this->render('admin/movie/list.html.twig');
+    }
+}
