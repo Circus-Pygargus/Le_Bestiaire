eb9eeb6fab7ea756309be50cbc9e8cd30a1a6389
Circus-Pygargus <richard.meuret@laposte.net>
2021-11-28 17:11:44 +0100

    [MONSTERS] Création de la route /admin/monstrers/list : contrôleur, vue et rôles

---
+++ src/Controller/Admin/MonsterController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class MonsterController
+ * @package App\Controller\Admin
+ * @Route("/admin/monsters", name="admin_monsters_")
+ */
+class MonsterController extends AbstractController
+{
+    /**
+     * @Route("list", name="list")
+     */
+    public function list (): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_CONTRIBUTOR');
+
+        return $this->render('admin/monster/list.html.twig');
+    }
+}
