cbaf10ee8096c09096f1eef68b6300370f90bc4f
Circus-Pygargus <richard.meuret@laposte.net>
2021-12-04 22:13:16 +0100

    [REFACTO] AdminController : ajout des propriétés protected $em et $logger

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,12 +2,26 @@
 
 namespace App\Controller\Admin;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
+    protected EntityManagerInterface $em;
+    protected LoggerInterface $logger;
+
+    public function __construct (
+        EntityManagerInterface $em,
+        LoggerInterface $logger
+    )
+    {
+        $this->em = $em;
+        $this->logger = $logger;
+    }
+
     /**
      * @Route("/admin", name="admin")
      */
