3ecd6aa88978005f029ebf96746a631aaae4cb7c
Circus-Pygargus <richard.meuret@laposte.net>
2021-12-04 22:21:49 +0100

    [CATEGORY] Ajout de la route /admin/category/delete/{id}

--- src/Controller/Admin/CategoryController.php
+++ src/Controller/Admin/CategoryController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Controller\Admin\AdminController;
 use App\Entity\Category;
 use App\Form\Category\CreateCategoryFormType;
 use App\Repository\CategoryRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,7 +15,7 @@ use Symfony\Component\Routing\Annotation\Route;
  * @package App\Controller\Admin
  * @Route("/admin/Categories", name="admin_categories_")
  */
-class CategoryController extends AbstractController
+class CategoryController extends AdminController
 {
     /**
      * @Route("/list", name="list")
@@ -71,5 +71,30 @@ class CategoryController extends AbstractController
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
         return $this->render('admin/category/edit.html.twig');
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete (CategoryRepository $categoryRepository, int $id): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_SUPER_ADMIN');
+
+        try {
+            $category = $categoryRepository->findOneBy(['id' => $id]);
+
+            $this->em->remove($category);
+            $this->em->flush();
+
+        } catch (\Exception $e) {
+            $this->logger->error($e->getMessage());
+            $this->logger->debug($e->getTraceAsString());
+
+            $this->addFlash('error', 'Un problème est survenu, la catégorie n\'a pas été supprimée !');
+            return $this->redirectToRoute('admin_categories_list');
+        }
+
+        $this->addFlash('success', 'La catégorie <strong>' . $category->getName() . '</strong> vient d\'être supprimée');
+        return $this->redirectToRoute('admin_categories_list');
     }
 }
