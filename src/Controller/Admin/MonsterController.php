24457f1dcada9e0375f30475ee508b7babbfe769
Circus-Pygargus <richard.meuret@laposte.net>
2021-12-05 12:27:48 +0100

    [MONSTER] Un contributeur peut éditer un monstre

--- src/Controller/Admin/MonsterController.php
+++ src/Controller/Admin/MonsterController.php
@@ -71,13 +71,42 @@ class MonsterController extends AdminController
     }
 
     /**
-     * @Route("/edit", name="edit")
+     * @Route("/edit/{slug}", name="edit")
      */
-    public function edit (): Response
+    public function edit (Request $request, MonsterRepository $monsterRepository, string $slug): Response
     {
         $this->denyAccessUnlessGranted('ROLE_CONTRIBUTOR');
 
-        return $this->render('admin/monster/edit.html.twig');
+        $monster = $monsterRepository->findOneBy(['slug' => $slug]);
+        if (!$monster) {
+            $this->addFlash('error', 'Le monstre <strong>' . $slug . '</strong> n\'existe pas');
+
+            return $this->redirectToRoute('admin_monsters_list');
+        }
+
+        $form = $this->createForm(MonsterFormType::class, $monster);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $this->em->persist($monster);
+                $this->em->flush();
+            } catch (\Exception $e) {
+                $this->logger->error($e->getMessage());
+                $this->logger->debug($e->getTraceAsString());
+
+                $this->addFlash('error', 'Un problème est survenu, le monstre n\'a pas été modifié !');
+                return $this->redirectToRoute('admin_monsters_list');
+            }
+
+            $this->addFlash('success', 'Le monstre <strong>' . $monster->getName() . '</strong> a bien été modifié.');
+
+            return $this->redirectToRoute('admin_monsters_list');
+        }
+
+        return $this->render('admin/monster/edit.html.twig', [
+            'monsterForm' => $form->createView()
+        ]);
     }
 
     /**
