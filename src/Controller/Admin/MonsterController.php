e0e0f508252107db914940112bdaba5cb748e2f5
Circus-Pygargus <richard.meuret@laposte.net>
2021-12-05 01:43:57 +0100

    [MONSTER] Un contributeur peut créer un nouveau monster

--- src/Controller/Admin/MonsterController.php
+++ src/Controller/Admin/MonsterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Admin;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Controller\Admin\AdminController;
+use App\Entity\Monster;
+use App\Form\Monster\MonsterFormType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
  * @package App\Controller\Admin
  * @Route("/admin/monsters", name="admin_monsters_")
  */
-class MonsterController extends AbstractController
+class MonsterController extends AdminController
 {
     /**
      * @Route("list", name="list")
@@ -26,11 +29,34 @@ class MonsterController extends AbstractController
     /**
      * @Route("/create", name="create")
      */
-    public function create (): Response
+    public function create (Request $request): Response
     {
         $this->denyAccessUnlessGranted('ROLE_CONTRIBUTOR');
 
-        return $this->render('admin/monster/create.html.twig');
+        $monster = new Monster();
+        $form = $this->createForm(MonsterFormType::class, $monster);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $this->em->persist($monster);
+                $this->em->flush();
+            } catch (\Exception $e) {
+                $this->logger->error($e->getMessage());
+                $this->logger->debug($e->getTraceAsString());
+
+                $this->addFlash('error', 'Un problème est survenu, le monstre n\'a pas été créé !');
+                return $this->redirectToRoute('admin_monsters_list');
+            }
+
+            $this->addFlash('success', 'Le monstre <strong>' . $monster->getName() . '</strong> a bien été créé.');
+
+            return $this->redirectToRoute('admin_monsters_list');
+        }
+
+        return $this->render('admin/monster/create.html.twig', [
+            'monsterForm' => $form->createView()
+        ]);
     }
 
     /**
