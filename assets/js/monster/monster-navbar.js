5069ff1df1a3e3d86f0a34a76c036894f5f7cdf3
Circus-Pygargus <richard.meuret@laposte.net>
2021-12-25 16:34:46 +0100

    [MONSTER] La navbar de contenu d'un monstre est fonctionnelle

--- assets/js/monster/monster-navbar.js
+++ assets/js/monster/monster-navbar.js
@@ -1,5 +1,24 @@
 const monsterNavbar = function () {
-    console.log('toto');
+
+    const monsterContentBtns = document.querySelectorAll('.monster-content-btn');
+    const monsterContentDiv = document.querySelector('#monster-content');
+    const monsterSubContentDivs = monsterContentDiv.querySelectorAll('.monster-content--sub-div');
+
+    monsterContentBtns.forEach(btn => {
+        if (!btn.classList.contains('disabled')) {
+            btn.addEventListener('click', () => {
+                monsterContentBtns.forEach(button => {
+                    button.classList.remove('active');
+                });
+                btn.classList.add('active');
+
+                monsterSubContentDivs.forEach(contentDiv => {
+                    contentDiv.classList.add('hidden');
+                });
+                monsterContentDiv.querySelector('[data-content="' + btn.dataset.target + '"]').classList.remove('hidden');
+            });
+        }
+    });
 };
 
 module.exports = monsterNavbar;
